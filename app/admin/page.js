037fddaef099171dc0c150a76fa6290c2f2efb5e
mariusaadland-byte <mariusaadland@gmail.com>
2026-09-18 02:04:52 +0200

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,13 +1,13 @@
 import { redirect } from "next/navigation";
-import { isAdmin } from "../../lib/auth";
+import { getAdminUser } from "../../lib/auth";
 import AdminClient from "./AdminClient";
 
 export default async function AdminPage() {
-  const admin = await isAdmin();
+  const admin = await getAdminUser();
 
   if (!admin) {
     redirect("/admin/login");
   }
 
-  return <AdminClient />;
+  return <AdminClient user={admin} />;
 }
