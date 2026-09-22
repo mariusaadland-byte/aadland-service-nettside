df3787b660f30cd48e20724a9848f811e6ce6958
mariusaadland-byte <mariusaadland@gmail.com>
2026-09-23 01:10:58 +0200

    Add customer quote page

---
+++ app/tilbud/[id]/page.js
@@ -0,0 +1,12 @@
+import QuoteCustomerClient from "./QuoteCustomerClient";
+
+export const metadata={
+ title:"Tilbud | Aadland Service",
+ robots:{index:false,follow:false}
+};
+
+export default async function CustomerQuotePage({params,searchParams}){
+ const resolvedParams=await params;
+ const resolvedSearch=await searchParams;
+ return <QuoteCustomerClient quoteId={resolvedParams.id} token={String(resolvedSearch?.token||"")}/>;
+}
