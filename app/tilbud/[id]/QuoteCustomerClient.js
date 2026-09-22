cf3c0cf11b506582e779fac485725d0a972ec1a3
mariusaadland-byte <mariusaadland@gmail.com>
2026-09-23 01:16:36 +0200

    Use Aadland Service logo in quote document

--- app/tilbud/[id]/QuoteCustomerClient.js
+++ app/tilbud/[id]/QuoteCustomerClient.js
@@ -65,8 +65,7 @@ export default function QuoteCustomerClient({quoteId,token}){
   <article className="quoteDocument customerQuoteDocument">
    <header className="quoteDocHeader">
     <div className="quoteDocBrand">
-     <div className="quoteDocMark">AS</div>
-     <div><b>AADLAND SERVICE</b><span>Håndverk · Vedlikehold · Utleie</span></div>
+     <img className="quoteDocLogo" src="/aadland-service-logo.webp" alt="Aadland Service"/>
     </div>
     <div className="quoteDocCompany">
      <b>Marius Aadland</b>
