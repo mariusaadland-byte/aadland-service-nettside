a064eae041d7256ca8057d23a9d8f7c3d22315be
mariusaadland-byte <mariusaadland@gmail.com>
2026-09-23 22:38:00 +0200

    Add production robots rules

--- app/robots.js
+++ app/robots.js
@@ -1,1 +1,18 @@
-export default function robots(){return {rules:[{userAgent:"*",allow:"/",disallow:["/admin","/api","/min-side"]}],sitemap:"https://www.aadland-service.no/sitemap.xml"}}
+export default function robots(){
+  const base="https://www.aadland-service.no";
+  return {
+    rules:[
+      {
+        userAgent:"*",
+        allow:"/",
+        disallow:[
+          "/admin/",
+          "/min-side/",
+          "/tilbud/"
+        ]
+      }
+    ],
+    sitemap:base+"/sitemap.xml",
+    host:base
+  };
+}
