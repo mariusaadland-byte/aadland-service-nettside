5991547dacb976a4d8b9ddc2c0f7bb87eafde2b6
mariusaadland-byte <mariusaadland@gmail.com>
2026-09-24 11:54:59 +0200

    Avoid duplicated site name in page titles

--- app/admin/tegning/page.js
+++ app/admin/tegning/page.js
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import { getAdminUser } from "../../../lib/auth";
 import DrawingClient from "./DrawingClient";
 
-export const metadata = { title: "Tegning & visualisering | Aadland Service" };
+export const metadata = { title: "Tegning & visualisering" };
 
 export default async function DrawingPage() {
   const admin = await getAdminUser();
